75c56325e3beec27702b2ab451e78b366d1612b3
Eden Reich <eden.reich@gmail.com>
2016-10-21 22:13:08 +0200

    Update chat.js

--- chat.js
+++ chat.js
@@ -34,7 +34,7 @@ if(typeof Object.create !== 'function') {
 | these all at once.
 |
 | This plugin is build for personal use and is absolutly restricted for a commerical use.
-| Copy Right Eden Reich, all rights reserved.
+| Copy Rights Eden Reich, all rights reserved.
 |
 */
 ;(function($, window, document, undefined) {
